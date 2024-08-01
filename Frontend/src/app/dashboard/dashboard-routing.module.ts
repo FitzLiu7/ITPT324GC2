bf839ecaf029e0299e4b039e478ec97e1997ef5d
Juan Camilo Serna Trejos <149125041+JuanCaSerTre@users.noreply.github.com>
2024-08-01 21:26:47 +1000

    setting up

--- Frontend/src/app/dashboard/dashboard-routing.module.ts
+++ Frontend/src/app/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OverallComponent } from './overall/overall.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'overall', component: OverallComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
