4f11bdc14e5479c5b8db6a1e2ebacafcbd461419
Juan Camilo Serna Trejos <149125041+JuanCaSerTre@users.noreply.github.com>
2024-08-01 21:31:26 +1000

    update

--- Frontend/src/app/shared/shared.module.ts
+++ Frontend/src/app/shared/shared.module.ts
@@ -2,14 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
-
-
 @NgModule({
-  declarations: [
-    SidebarComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SidebarComponent],
+  imports: [CommonModule],
 })
-export class SharedModule { }
+export class SharedModule {}
