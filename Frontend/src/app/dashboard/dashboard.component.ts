a9ba6b0357b64419c6a7e6141a29f7e6edfd15b6
Juan Camilo Serna Trejos <149125041+JuanCaSerTre@users.noreply.github.com>
2024-08-29 00:01:12 +1000

    ui updated

--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ApiService } from '../api.service';
 
 interface Room {
-  Date: string;
-  Stock: number;
-  Week: number;
-  FoodType: string;
-  Tubs: string;
-  WaterType: string;
-  RoomNumber: number;
+  Date?: string;
+  Stock?: number;
+  Week?: number;
+  FoodType?: string;
+  Tubs?: string;
+  WaterType?: string;
+  RoomNumber: number | string;
+  Stage?: string;
 }
 
 @Component({
@@ -21,31 +22,53 @@ interface Room {
 })
 export default class DashboardComponent implements OnInit {
   rooms: Room[] = [];
+  fixedRooms: (number | string)[] = [
+    'N1',
+    'N2',
+    1,
+    3,
+    4,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+  ];
 
   constructor(private apiService: ApiService) {}
-  
+
   ngOnInit() {
     // Fetch initial data from HTTP request
     this.apiService.getList().subscribe(
       (initialData) => {
         console.log('Initial data:', initialData);
-        this.rooms = initialData; // Initialize rooms with HTTP data
+        this.populateRooms(initialData);
       },
       (error) => {
         console.error('Error fetching initial data:', error);
       }
     );
-  
+
     // Subscribe to the WebSocket updates
     this.apiService.getDataUpdates().subscribe(
       (data) => {
         console.log('Updated data:', data);
-        this.rooms = data; // Update rooms with WebSocket data
+        this.populateRooms(data);
       },
       (error) => {
         console.error('Error receiving WebSocket data:', error);
       }
     );
   }
-  
+
+  populateRooms(data: Room[]) {
+    this.rooms = this.fixedRooms.map((roomNumber) => {
+      const existingRoom = data.find((room) => room.RoomNumber === roomNumber);
+      return existingRoom
+        ? existingRoom
+        : { RoomNumber: roomNumber, Stock: 0, Week: 0, Stage: 'Unknown' };
+    });
+  }
 }
--- Frontend/src/app/foodandhydration/foodandhydration.component.ts
+++ Frontend/src/app/foodandhydration/foodandhydration.component.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ApiService } from '../api.service';
 
 interface Room {
-  Date: string;
-  Stock: number;
-  Week: number;
-  FoodType: string;
-  Tubs: string;
-  WaterType: string;
-  RoomNumber: string;
-  Stage: string;
+  Date?: string;
+  Stock?: number;
+  Week?: number;
+  FoodType?: string;
+  Tubs?: string;
+  WaterType?: string;
+  RoomNumber: number | string;
+  Stage?: string;
 }
 
 @Component({
@@ -21,158 +21,42 @@ interface Room {
   styleUrls: ['./foodandhydration.component.css'],
 })
 export class FoodandhydrationComponent implements OnInit {
-  allRooms: Room[] = [
-    {
-      RoomNumber: 'N1',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-    {
-      RoomNumber: 'N2',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-    {
-      RoomNumber: '1',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-    {
-      RoomNumber: '3',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-    {
-      RoomNumber: '4',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-    {
-      RoomNumber: '8',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-    {
-      RoomNumber: '9',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-    {
-      RoomNumber: '10',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-    {
-      RoomNumber: '11',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-    {
-      RoomNumber: '12',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-    {
-      RoomNumber: '13',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-    {
-      RoomNumber: '14',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-    {
-      RoomNumber: '15',
-      Date: '',
-      Stock: 0,
-      Week: 0,
-      FoodType: '',
-      Tubs: '',
-      WaterType: '',
-      Stage: '',
-    },
-  ];
-
   rooms: Room[] = [];
+  fixedRooms: (number | string)[] = [
+    'N1',
+    'N2',
+    1,
+    3,
+    4,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+  ];
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
+    // Fetch initial data from HTTP request
     this.apiService.getList().subscribe(
       (initialData) => {
         console.log('Initial data:', initialData);
-        this.rooms = this.processRoomsData(initialData);
+        this.populateRooms(initialData);
       },
       (error) => {
         console.error('Error fetching initial data:', error);
       }
     );
 
+    // Subscribe to the WebSocket updates
     this.apiService.getDataUpdates().subscribe(
       (data) => {
         console.log('Updated data:', data);
-        this.rooms = this.processRoomsData(data);
+        this.populateRooms(data);
       },
       (error) => {
         console.error('Error receiving WebSocket data:', error);
@@ -180,14 +64,25 @@ export class FoodandhydrationComponent implements OnInit {
     );
   }
 
-  private processRoomsData(rooms: Room[]): Room[] {
-    return rooms.map((room) => ({
-      ...room,
-      Stage: this.calculateStage(room.Date),
-    }));
+  populateRooms(data: Room[]) {
+    this.rooms = this.fixedRooms.map((roomNumber) => {
+      const existingRoom = data.find((room) => room.RoomNumber === roomNumber);
+      return existingRoom
+        ? { ...existingRoom, Stage: this.calculateStage(existingRoom.Date) }
+        : {
+            RoomNumber: roomNumber,
+            Stock: 0,
+            Week: 0,
+            Stage: 'Unknown',
+            FoodType: '',
+            WaterType: '',
+          };
+    });
   }
 
-  private calculateStage(stockDate: string): string {
+  private calculateStage(stockDate?: string): string {
+    if (!stockDate) return 'Unknown';
+
     const startDate = new Date(stockDate);
     const currentDate = new Date();
     const weeksDiff = this.getWeeksDifference(startDate, currentDate);
